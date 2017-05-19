17bf50335fbba022dc7744a719d63e51fc152047
GorgeousOne <aaron.postfach@gmx.de>
2017-05-19 15:01:43 +0200

    full grid background

--- src/view/drawables/Background.java
+++ src/view/drawables/Background.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 
 import view.window.Camera;
@@ -28,34 +27,56 @@ public class Background extends Drawable{
 		this.camera = camera;
 		
 		setScale(fWidth/width, fWidth/width);
+		
 	}
 	
 	@Override
 	public void fill(Graphics g) {
 		
 		Graphics2D g2 = (Graphics2D) g;
-		AffineTransform old = g2.getTransform();
-
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); 
 		
 		g2.setColor(Drawable.DARK_GRAY);
 		g2.fillRect(0, (int) (camera.getY() - fHeight/2/camera.getZoom()), fWidth, (int) (fHeight/camera.getZoom()));
+		
+		//fHeight, bloss auf den camera-zoom angepasst
+		int sHeight = (int) (fHeight / camera.getZoom());
+		//Auslenkung des Hintergrunds bei x-Bewegung des Balls
+		int sShake = (int) (camera.x-fWidth/2) / 4;
+		
+		//Wert, bei dem Tiles angefangen werden zu malen; das wird immer x TilesLaenge genommen
+		double tilesY = Math.floor((camera.y - sHeight/2) / (height * getScale().getX()));
+		
+		//Hintergrund nicht ueber Raender malen
+		g2.clipRect(0, (int) (camera.y - sHeight/2), fWidth, sHeight);
+		g2.setColor(Drawable.MIDDLE_GRAY);
+		
+		while(tilesY * height*getScale().getX() < camera.y + sHeight/2) {
+			g2.translate( fWidth/2 + sShake,  tilesY * height*getScale().getX());
+			g2.fill(getShape());
+			g2.translate(-fWidth/2 - sShake, -tilesY * height*getScale().getX());
 
+			tilesY++;
+		}
+		
+		double gridX = (int) (-sShake / getScale().getX());
+		double gridY = Math.floor((camera.y - sHeight/2) / getScale().getX());
+		
 		g2.setColor(Drawable.GRAY);
 		
-		int sHeight = (int) (fHeight / camera.getZoom());
-		double tilesY = Math.floor((camera.y - sHeight/2) / (height * getScale().getX()));
-
-		while(tilesY * height*getScale().getX() < camera.y + sHeight/2) {
-			
-			g2.translate(fWidth/2,  tilesY  * height*getScale().getX());
-			g2.fill(getShape());
-			g2.translate(-fWidth/2, -tilesY  * height*getScale().getX());
-			
-			tilesY ++;
+		while(gridX * getScale().getX() < fWidth - sShake) {
+			g2.drawLine((int) (gridX * getScale().getX()) + sShake, (int) (camera.y - sHeight/2), 
+						(int) (gridX * getScale().getX()) + sShake, (int) (camera.y + sHeight/2));
+			gridX += 1;
+		}
+		
+		while(gridY * height*getScale().getX() < camera.y + sHeight) {
+			g2.drawLine(0, (int) (gridY * getScale().getX()), 
+				   fWidth, (int) (gridY * getScale().getX()));
+			gridY++;
 		}
 
-		g2.setTransform(old);
+		g2.setClip(null);
 	}
 	
 	public static Area initTiles2() {
