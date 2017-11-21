459fb49ba8b0337d11277478567f920f5650b4ec
GorgeousOne <aaron.postfach@gmx.de>
2017-11-21 12:44:06 +0100

    Add files via upload

--- src/view/drawables/Explosion.java
+++ src/view/drawables/Explosion.java
@@ -12,14 +12,18 @@ public class Explosion extends Drawable {
 
 	private long duration;
 	private ArrayList<Particle> particles;
-	private boolean isLaunched = false;
+	private boolean isLaunched;
 	private int pCount;
 	private float pSpeed, pFriction;
 	
 	public Explosion(Area shape, int layer) {
 		super(shape, layer);
 		
+		isLaunched = false;
 		particles = new ArrayList<>();
+		pCount = 10;
+		pSpeed = 1f;
+		pFriction = 1f;
 	}
 
 	public boolean isLaunched() {
@@ -34,7 +38,7 @@ public class Explosion extends Drawable {
 		isLaunched = true;
 		
 		for(int i = 0; i < pCount; i++) {
-			Particle p = new Particle(createShape(), pSpeed, pFriction);
+			Particle p = new Particle(createShape(), solids);
 			p.setScale(getScale());
 			p.setPos((Point2D) getPos().clone());
 			p.setColor(getColor());
@@ -51,7 +55,7 @@ public class Explosion extends Drawable {
 						p.update(solids);
 					
 					try {
-						Thread.sleep(16);
+						Thread.sleep(16);	//TODO add variable
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -62,7 +66,7 @@ public class Explosion extends Drawable {
 		t.start();
 	}
 	
-	public void setParticles(int count) {
+	public void setCount(int count) {
 		pCount = (int) Math.max(1, count);
 	}
 	
@@ -91,25 +95,26 @@ public class Explosion extends Drawable {
 	private class Particle extends Drawable {
 
 		private double spin;
-		private Vector2D speed;
+		private Vector2D velocity;
 		private double friction;
 		
-		public Particle(Area shape, float speed, float friction) {
+		public Particle(Area shape, ArrayList<Drawable> solids) {
 			super(shape, 0);
 			
 			setRotation(Math.random() * 2*Math.PI);
-			speed -= Math.random()*speed/2;
-//			this.spin = Math.random() * Math.PI/4 - Math.PI/8;
+			double speed = Explosion.this.pSpeed;
+			speed += Math.random()*speed - speed/2;
+			
 			this.spin = Math.random() * speed/4/Math.PI - speed/8/Math.PI;
-			this.friction = friction;
-			this.speed = new Vector2D(Math.cos(getRotation()) * speed, 
+			this.friction = Explosion.this.pFriction;
+			this.velocity = new Vector2D(Math.cos(getRotation()) * speed, 
 									  Math.sin(getRotation()) * speed);
 		}
 		
 		public void update(ArrayList<Drawable> solids) {
 
-			translate(speed.getX(), speed.getY());
-			rotate(spin * speed.getLength());
+			translate(velocity.getX(), velocity.getY());
+			rotate(spin * velocity.getLength());
 			
 			for(Drawable d : solids) {
 				
@@ -118,23 +123,23 @@ public class Explosion extends Drawable {
 				
 				if(!intersect.isEmpty()) {
 					
-					translate(-speed.getX(), -speed.getY());
-					rotate(-spin * speed.getLength());
+					translate(-velocity.getX(), -velocity.getY());
+					rotate(-spin * velocity.getLength());
 					
 					Point2D center = new Point2D.Double(intersect.getBounds2D().getCenterX(),
 														intersect.getBounds2D().getCenterY());
 					
-					double pushAngle = getAngleBetween(getAngleTo(getPos(), center));
+					double impactAngle = getAngleBetween(getAngleTo(getPos(), center));
 					double radius = getPos().distance(center);
 					
-					speed.multiply(Math.cos(pushAngle));
-					speed.negate().rotate(2*pushAngle);
+					velocity.multiply(Math.cos(impactAngle));
+					velocity.negate().rotate(2*impactAngle);
 
-					spin += Math.sin(pushAngle) / radius;
+					spin += Math.sin(impactAngle) / radius;
 				}
 			}
 			
-			speed.multiply(1.0/friction);
+			velocity.multiply(1.0/friction);
 		}
 		
 		private double getAngleTo(Point2D p, Point2D p2) {
@@ -151,7 +156,7 @@ public class Explosion extends Drawable {
 		}
 		
 		private double getAngleBetween(double theta) {
-			double angle = theta - speed.getAngle();
+			double angle = theta - velocity.getAngle();
 			
 			if(Math.abs(angle) > Math.PI)
 				angle -= Math.signum(angle) * 2*Math.PI;
